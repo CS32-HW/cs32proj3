f264977379052161208eea534050b58a12ad2d12
Ethan Cheng <ethan@nijika.org>
2024-02-22 12:58:11 -0800

    change direction of Avatar when moving

--- src/Actor.cpp
+++ src/Actor.cpp
@@ -8,15 +8,19 @@ void Avatar::move()
 		switch (key) {
 		case KEY_PRESS_LEFT:
 			moveTo(getX() - 1, getY());
+			setDirection(180);
 			break;
 		case KEY_PRESS_RIGHT:
 			moveTo(getX() + 1, getY());
+			setDirection(0);
 			break;
 		case KEY_PRESS_UP:
 			moveTo(getX(), getY() + 1);
+			setDirection(90);
 			break;
 		case KEY_PRESS_DOWN:
 			moveTo(getX(), getY() - 1);
+			setDirection(270);
 			break;
 		}
 	}
